03860505bddf32bcc4f71092734e983912239351
Peter <peter.zmilczak@gmail.com>
2022-11-18 22:05:29 +0100

    starting repo google oauth backend

---
+++ src/main/java/com/codecool/marwin1991/SpringLogin.java
@@ -0,0 +1,11 @@
+package com.codecool.marwin1991;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogin {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLogin.class, args);
+	}
+}
